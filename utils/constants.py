6d6960e0ac7c1c6f38a3ec89e7aae78ac61d2911
JuanD <dosquisd123@gmail.com>
2025-08-18 15:13:29 -0500

    feat: calc new metrics

--- utils/constants.py
+++ utils/constants.py
@@ -5,8 +5,8 @@ the NMDB (Neutron Monitor Database) analysis pipeline.
 """
 
 import nolds
+import antropy as ant
 from scipy.stats import entropy
-from antropy import higuchi_fd, perm_entropy
 
 import numpy as np
 from typing import Dict, TypedDict, Callable, Any, Literal
@@ -14,11 +14,12 @@ from typing import Dict, TypedDict, Callable, Any, Literal
 
 class MetricDetails(TypedDict):
     """Type definition for metric configuration.
-    
+
     Attributes:
         func: The function to calculate the metric.
         kwargs: Keyword arguments to pass to the function.
     """
+
     func: Callable[..., np.float64 | np.ndarray]
     kwargs: Dict[str, Any]
 
@@ -27,29 +28,35 @@ WINDOW_SIZE = 130  # 1 unit === 1 minute each window
 
 # Dictionary containing all available metrics for analysis
 METRICS: Dict[str, MetricDetails] = {
-    # Scipy metrics
+    # Entropy
     "entropy": {
         "func": lambda x, kwargs: entropy(x, **kwargs),
         "kwargs": {},
     },
-
-    # AntroPy metrics
-    "permutation_entropy": {
-        "func": lambda x, kwargs: perm_entropy(x, **kwargs),
-        "kwargs": {"normalize": False, "order": 3, "delay": 1},  # Default parameters
-    },
-    "higuchi": {
-        "func": lambda x, kwargs: higuchi_fd(x, **kwargs),
-        "kwargs": {"kmax": 10},
-    },
-    # Search for more metrics
-
-    # Nolds metrics. Maybe, will be necessary to change each metric kwargs
-    # e.g. order for dfa, emb_dim using elbow method, etc.
     "sampen": {
         "func": lambda x, kwargs: nolds.sampen(x, **kwargs),
         "kwargs": {},
     },
+    "permutation_entropy": {
+        "func": lambda x, kwargs: ant.perm_entropy(x, **kwargs),
+        "kwargs": {"normalize": False, "order": 3, "delay": 1},  # Default parameters
+    },
+    "shannon_entropy": {
+        "func": lambda x, kwargs: entropy(
+            np.histogram(x, bins="auto", density=True)[0], **kwargs
+        ),
+        "kwargs": {},
+    },
+    "spectral_entropy": {
+        "func": lambda x, kwargs: ant.spectral_entropy(x, **kwargs),
+        "kwargs": {"method": "welch", "sf": 0.1, "normalize": True},
+    },
+    "app_entropy": {
+        "func": lambda x, kwargs: ant.app_entropy(x, **kwargs),
+        "kwargs": {},
+    },
+
+    # Hurst exponent
     "hurst": {
         "func": lambda x, kwargs: nolds.hurst_rs(x, **kwargs),
         "kwargs": {},
@@ -60,30 +67,39 @@ METRICS: Dict[str, MetricDetails] = {
         # and orverlap=False add a lot of noise
         "kwargs": {"order": 2, "overlap": True},
     },
-    "corr_dim": {
-        "func": lambda x, kwargs: nolds.corr_dim(x, **kwargs),
-        "kwargs": {"emb_dim": 2},
+    "mfhurst_b": {
+        "func": lambda x, kwargs: nolds.mfhurst_b(x, **kwargs),
+        "kwargs": {},
     },
+
+    # Fractal dimension
+    "higuchi_fd": {
+        "func": lambda x, kwargs: ant.higuchi_fd(x, **kwargs),
+        "kwargs": {"kmax": 10},
+    },
+    "katz_fd": {
+        "func": lambda x, kwargs: ant.katz_fd(x, **kwargs),
+        "kwargs": {},
+    },
+    "petrosian_fd": {
+        "func": lambda x, kwargs: ant.petrosian_fd(x, **kwargs),
+        "kwargs": {},
+    },
+    "lepel_ziv": {
+        "func": lambda x, kwargs: ant.lziv_complexity(np.array(x), **kwargs),
+        "kwargs": {},
+    },
+
+    # Chaos indicators
     "lyap_r": {
         "func": lambda x, kwargs: nolds.lyap_r(x, **kwargs),
         "data": [],
         "kwargs": {},
     },
-    # This returns a vector of `matrix_dim`. I'll skip it meanwhile
-    # "lyap_e": {
-    #     "func": lambda x, kwargs: nolds.lyap_e(x, **kwargs),
-    #     # `emb_dim` - 1 must be divisible by `matrix_dim` - 1
-    #     "kwargs": {"emb_dim": 10, "matrix_dim": 4},  # Default parameters
-    # },
-    "mfhurst_b": {
-        "func": lambda x, kwargs: nolds.mfhurst_b(x, **kwargs),
-        "kwargs": {},
+    "corr_dim": {
+        "func": lambda x, kwargs: nolds.corr_dim(x, **kwargs),
+        "kwargs": {"emb_dim": 2},
     },
-    # This returns a vector too. I'll skip it meanwhile
-    # "mfhurst_dm": {
-    #     "func": lambda x, kwargs: nolds.mfhurst_dm(x, **kwargs),
-    #     "kwargs": {},
-    # },
 }
 
 # Type alias for supported event types
